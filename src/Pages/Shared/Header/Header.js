c3019ef17f9ea8c0352f2699d290112e9d75df6d
Sajib Debnath <debnathsajib2024@gmail.com>
2023-08-07 17:03:21 +0530

    first commit

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,68 @@
+import React, { useContext } from 'react';
+import { Image } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
+    return (
+        <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand><Link to='/'>Dragon News</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">All News</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <>
+                            {
+                                user?.uid ?
+                                    <>
+                                        <span >{user?.displayName}</span>
+                                        <span style={{ cursor: 'pointer',  }} className='mx-3 ' variant="light" onClick={handleLogOut}>Log out</span>
+                                    </>
+                                    :
+                                    <>
+                                        <Link to='/login'>Login</Link>
+                                        <Link to='/register'>Register</Link>
+                                    </>
+                            }
+
+
+                        </>
+                        <Link to="/profile">
+                            {user?.photoURL ?
+                                <Image
+                                    style={{ height: '30px' }}
+                                    roundedCircle
+                                    src={user?.photoURL}>
+                                </Image>
+                                : <FaUser></FaUser>
+                            }
+                        </Link>
+                    </Nav>
+                    <div className='d-lg-none'>
+                        <LeftSideNav></LeftSideNav>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1y_DO8vixuo6EPN8qOz6WxHtMWS3QAsw",
+  authDomain: "dragon-news-f7835.firebaseapp.com",
+  projectId: "dragon-news-f7835",
+  storageBucket: "dragon-news-f7835.appspot.com",
+  messagingSenderId: "116447847554",
+  appId: "1:116447847554:web:2e9fefdcba79769927009b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
